4508fdf2db6fb4a43d653fcb94693fa9f93e8854
dacame <david@venera7.com>
2015-03-09 16:33:42 +0100

    Less aaa's
    Oh yeah better

--- TestApp/src/main/java/com/globetesting/com/App.java
+++ TestApp/src/main/java/com/globetesting/com/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello My World!" );
-        System.out.println( "Ohhh yeeeaaaaah!" );
+        System.out.println( "Ohhh yeeeah!" );
     }
 }
